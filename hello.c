b10c337561845dee97cfea788a318fe8a860f3f4
Dwij <gosaidwij045@gmail.com>
2025-06-12 20:00:28 +0530

    add isprime function to hello.c for understanding rebase

--- hello.c
+++ hello.c
@@ -6,6 +6,16 @@ void even1(int a){
     else
         printf("%d is odd number\n", a);
 }
+void isprime(int a){
+    int i;
+    for(i=2; i<=a/2; i++){
+        if(a%i==0){
+            printf("%d is not a prime number\n", a);
+            return;
+        }
+    }
+    printf("%d is a prime number\n", a);
+}
 int main()
 {
     printf("Hello World\n");
