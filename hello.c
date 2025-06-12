28c92ee04931458c6874f60c9db0dae6c17c53bf
Dwij <gosaidwij045@gmail.com>
2025-06-12 19:35:19 +0530

    using stash

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+
+void even1(int a){
+    if(a%2==0)
+        printf("%d is even number\n", a);
+    else
+        printf("%d is odd number\n", a);
+}
 int main()
 {
     printf("Hello World\n");
+    even1(11);
     return 0;
 }
